77f9f3c00d35ee928ea6cbc506632484c176f999
masa0902dev <masa0902dev@gmail.com>
2025-02-24 23:46:24 +0900

    useEffectのref監視が不要だった

--- src/app/_components/Presenter.tsx
+++ src/app/_components/Presenter.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, use, useRef } from "react";
+import React, { FC, useEffect, use } from "react";
 import { Article } from "@/_context/types";
 
 type Props2 = {
@@ -15,14 +15,13 @@ export const Presenter: FC<Props2> = ({
 }) => {
   const articles =
     articlesPromise instanceof Promise ? use(articlesPromise) : articlesPromise;
-  const isPushed = useRef(false); // 初回の `pushArticles` 実行を記録
+  // const isPushed = useRef(false); // 初回の `pushArticles` 実行を記録
 
   useEffect(() => {
-    if (!isPushed.current && articles.length > 0) {
-      pushArticles(articles);
-      isPushed.current = true; // 2回目以降の実行を防ぐ
-    }
-  }, [articles, pushArticles]); // 依存配列を空にして、初回レンダリング時にのみ発火
+    pushArticles(articles);
+    // 依存配列を空にして、初回レンダリング時にのみ発火
+    // 今回は重複無しだが、重複ありでも問題なかった
+  }, []);
 
   return (
     <div style={{ marginBottom: "2rem" }}>
